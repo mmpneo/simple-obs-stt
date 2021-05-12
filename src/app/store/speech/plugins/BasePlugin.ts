8f800f7195386dc0132b46e5fc6169ef8ec43a18
mmpneo <mmpneo@gmail.com>
2021-05-12 12:50:50 +0300

    better error handling and reconnection

--- src/app/store/speech/plugins/BasePlugin.ts
+++ src/app/store/speech/plugins/BasePlugin.ts
@@ -4,6 +4,8 @@ import {ConnectionState}          from "../../../utils/types";
 export class BasePlugin {
   public readonly onFinal$: Subject<string>                          = new Subject<string>();
   public readonly onInter$: Subject<string>                          = new Subject<string>();
+  public readonly onNotification$: Subject<string>                   = new Subject<string>();
+  public readonly onPluginCrashed$: Subject<string>                  = new Subject<string>();
   public readonly onStatusChanged$: BehaviorSubject<ConnectionState> = new BehaviorSubject<ConnectionState>(ConnectionState.Disconnected);
 
   async Start(language: string, data: string[]) {
@@ -15,5 +17,7 @@ export class BasePlugin {
     this.onStatusChanged$.next(ConnectionState.Disconnected);
     this.onFinal$.complete();
     this.onInter$.complete();
+    this.onNotification$.complete();
+    this.onPluginCrashed$.complete();
   };
 }
