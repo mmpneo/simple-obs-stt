1ddedba6161ab04701ec8bbeba7bbbc6541b28d7
mmpneo <mmpneo@gmail.com>
2021-05-12 12:51:07 +0300

    native rendering

--- src/zone-config.ts
+++ src/zone-config.ts
@@ -6,7 +6,7 @@ if (!window) {
   (window as any).__Zone_disable_IntersectionObserver = true;
   // (window as any).__Zone_disable_customElements = true;
   (window as any).__Zone_disable_EventTarget = true;
-  // (window as any).__Zone_disable_queueMicrotask = true;
+  (window as any).__Zone_disable_queueMicrotask = true;
   (window as any).__Zone_disable_geolocation = true;
   (window as any).__Zone_disable_canvas = true;
   (window as any).__zone_symbol__DISABLE_WRAPPING_UNCAUGHT_PROMISE_REJECTION = true;
