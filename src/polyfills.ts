6d86ca1204195ba7a383bea1db87a05b4838b6a2
mmpneo <mmpneo@gmail.com>
2021-05-13 11:21:09 +0300

    angular 12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,7 +58,7 @@
  * Zone JS is required by default for Angular itself.
  */
 import './zone-config';
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 import 'zone.js/plugins/zone-patch-rxjs'
 
 
