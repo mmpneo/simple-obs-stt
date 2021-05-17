c7b6e0c29202856a0284d2723f0bcc36b3c95c43
mmpneo <mmpneo@gmail.com>
2021-05-18 00:02:46 +0300

    tw client_id fields

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  tw_client: '',
   production: true,
   localhostClientPath: 'http://localhost:3030',
   remoteClientPath: 'https://mmpneo.github.io/simple-obs-stt'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
+  tw_client: '',
   production: false,
-  localhostClientPath: 'http://localhost:3030',
+  localhostClientPath: 'http://localhost:4200',
   remoteClientPath: 'https://mmpneo.github.io/simple-obs-stt'
 };
 
