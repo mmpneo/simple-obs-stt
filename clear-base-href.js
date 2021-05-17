a4321d3ae048a3c744c1212d78156bf3542dfe84
mmpneo <mmpneo@gmail.com>
2021-05-18 00:00:39 +0300

    remove

--- clear-base-href.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const util = require('util');
-(async () => {
-  const file = await util.promisify(fs.readFile)("./dist/index.html", "utf8");
-  const str = file.replace(/<base href.*">/, "<base href=\"/\">");
-  fs.writeFileSync("./dist/index.html", str)
-})();
