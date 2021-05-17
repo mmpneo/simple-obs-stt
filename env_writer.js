3e43c9f896f8a0c9f39fc65462f5c1fe6b639d5f
mmpneo <mmpneo@gmail.com>
2021-05-18 00:25:08 +0300

    fix build

--- env_writer.js
+++ env_writer.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const targetPath    = './src/environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
-    tw_client: ${process.env.TW_CLIENT},
+    tw_client: '${process.env.TW_CLIENT}',
     production: true,
     localhostClientPath: 'http://localhost:3030',
     remoteClientPath: 'https://mmpneo.github.io/simple-obs-stt'
