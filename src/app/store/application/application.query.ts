4af486bbeb7a6bc8445b025b627911f144c72232
mmpneo <mmpneo@gmail.com>
2021-06-08 20:34:35 +0300

    refactor states

--- src/app/store/application/application.query.ts
+++ src/app/store/application/application.query.ts
@@ -1,26 +1,12 @@
 import {Injectable}                         from '@angular/core';
-import {combineQueries, Query}              from '@datorama/akita';
+import {Query}                              from '@datorama/akita';
 import {ApplicationState, ApplicationStore} from './application.store';
-import {NetworkQuery}                       from "@store/network/network.query";
-import {SpeechQuery}                        from "@store/speech/speech.query";
-import {map}                                from "rxjs/operators";
-import {ConnectionState}                    from "../../utils/types";
 
 @Injectable({providedIn: 'root'})
 export class ApplicationQuery extends Query<ApplicationState> {
-  constructor(protected store: ApplicationStore, private networkQuery: NetworkQuery, private speechQuery: SpeechQuery) {
+  constructor(protected store: ApplicationStore) {
     super(store);
   }
 
   theme$ = this.select('theme');
-
-  connectionState$ = combineQueries([this.networkQuery.state$, this.speechQuery.state$]).pipe(
-    map(([networkQuery, speechQuery]) => {
-      if (networkQuery.peerConnectionState === ConnectionState.Connected && speechQuery.speechServiceState === ConnectionState.Connected)
-        return ConnectionState.Connected;
-      if (networkQuery.peerConnectionState === ConnectionState.Disconnected && speechQuery.speechServiceState === ConnectionState.Disconnected)
-        return ConnectionState.Disconnected
-      return ConnectionState.Connecting;
-    })
-  )
 }
