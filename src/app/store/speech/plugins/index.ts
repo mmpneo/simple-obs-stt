3e85546e680446a8d02f62ca719561571bc62f82
mmpneo <mmpneo@gmail.com>
2021-06-11 00:56:23 +0300

    better plugin data fields

--- src/app/store/speech/plugins/index.ts
+++ src/app/store/speech/plugins/index.ts
@@ -9,6 +9,8 @@ export type SpeechPluginDescriptor = {
     name: string,
     plugin: { new(): BasePlugin },
     pluginDataFields: string[],
+    pluginDataFieldsType: string[],
+    pluginDataFieldsDescription: string[],
     platformValidate: () => boolean
   }
 }
@@ -18,18 +20,24 @@ export const SPEECH_PLUGINS: SpeechPluginDescriptor = {
     name:           'Native (Chrome and Edge)',
     plugin:         SpeechPluginNative,
     pluginDataFields: [],
+    pluginDataFieldsType: [],
+    pluginDataFieldsDescription: [],
     platformValidate: () => environment.platform === "web"
   },
   "noop": {
     name:           'Noop (Keyboard input only)',
     plugin:         SpeechPluginNoop,
     pluginDataFields: [],
+    pluginDataFieldsType: [],
+    pluginDataFieldsDescription: [],
     platformValidate: () => true
   },
   "azure":  {
     name:           "Azure Cognitive Services",
     plugin:         SpeechPluginAzure,
-    pluginDataFields: ["Service key", "Service location"],
+    pluginDataFields: ["Service key", "Service location", "Silence Timeout"],
+    pluginDataFieldsType: ["password", "password", "number"],
+    pluginDataFieldsDescription: ["Api key (KEY 1 or KEY 2 from your resource)", "Resource location", "Amount of seconds of silence required to put azure in sleeping mode. Default is 20 seconds"],
     platformValidate: () => true
   }
 }
