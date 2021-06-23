1a3eccc6267c57c1a33e5616464d05801617bced
mmpneo <mmpneo@gmail.com>
2021-06-23 19:07:00 +0300

    Move mute option to sound system

--- src/app/store/sound/sound.store.ts
+++ src/app/store/sound/sound.store.ts
@@ -1,18 +1,21 @@
 import {Injectable}         from '@angular/core';
 import {Store, StoreConfig} from '@datorama/akita';
+import produce              from "immer";
 
 export interface SoundState {
-  isVoicePlaying: boolean
+  isVoicePlaying: boolean,
+  mute: boolean
 }
 
 export function createInitialState(): SoundState {
   return {
-    isVoicePlaying: false
+    isVoicePlaying: false,
+    mute: false
   };
 }
 
 @Injectable({providedIn: 'root'})
-@StoreConfig({name: 'sound'})
+@StoreConfig({name: 'sound', producerFn: produce})
 export class SoundStore extends Store<SoundState> {
 
   constructor() {
