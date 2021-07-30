0ba87ecc8ab45e6c4128f911a4d8a5345c9dec02
mmpneo <mmpneo@gmail.com>
2021-07-30 04:18:54 +0300

    test

--- src/app/app.init.ts
+++ src/app/app.init.ts
@@ -15,21 +15,21 @@ export function InitLoading(fontsService: FontsService,): () => Promise<boolean>
     navigator.serviceWorker?.getRegistrations()?.then( function(registrations) { for(let registration of registrations) { registration.unregister(); } });
     try {
       await fontsService.LoadFonts();
-      await new Promise((res, rej) => {
-        if ((GetClientType() === ClientType.host) && !!window?.speechSynthesis)
-          window.speechSynthesis.onvoiceschanged = () => {
-            const voices                     = window.speechSynthesis.getVoices();
-            const groups                     = groupBy(voices, e => e.lang)
-            const langs                      = Object.keys(groups);
-            const voiceGroups                = langs.map(lang => [lang, lang, groups[lang].map(voice => [voice.name, voice.voiceURI])]);
-            (<any>window).NativeVoices       = voices;
-            (<any>window).NativeVoicesGroups = voiceGroups;
-            res(null);
-          }
-        else {
-          res(null);
-        }
-      })
+      // await new Promise((res, rej) => {
+      //   if ((GetClientType() === ClientType.host) && !!window?.speechSynthesis)
+      //     window.speechSynthesis.onvoiceschanged = () => {
+      //       const voices                     = window.speechSynthesis.getVoices();
+      //       const groups                     = groupBy(voices, e => e.lang)
+      //       const langs                      = Object.keys(groups);
+      //       const voiceGroups                = langs.map(lang => [lang, lang, groups[lang].map(voice => [voice.name, voice.voiceURI])]);
+      //       (<any>window).NativeVoices       = voices;
+      //       (<any>window).NativeVoicesGroups = voiceGroups;
+      //       res(null);
+      //     }
+      //   else {
+      //     res(null);
+      //   }
+      // })
       return true;
     } catch (error) {
       return true;
