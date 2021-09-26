a0b3499066f80fc74a14c632813151710db75b69
mmpneo <mmpneo@gmail.com>
2021-09-26 23:16:32 +0300

    zone update

--- src/zone-config.ts
+++ src/zone-config.ts
@@ -5,14 +5,39 @@ if (!window) {
   (window as any).__Zone_disable_MutationObserver = true;
   (window as any).__Zone_disable_IntersectionObserver = true;
   // (window as any).__Zone_disable_customElements = true;
-  (window as any).__Zone_disable_EventTarget = true;
+  (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove', 'click', 'mouseover', 'mouseout', 'mouseenter', 'mouseleave', 'resize', 'keypress'];
+
+  (window as any).__zone_symbol__ = true;
+  (window as any).__Zone_disable_EventEmitter = true;
+  (window as any).__Zone_disable_fs = true;
+  (window as any).__Zone_disable_node_timers = true;
+  (window as any).__Zone_disable_nextTick = true;
+  (window as any).__Zone_disable_crypto = true;
+  (window as any).__Zone_disable_defineProperty = true;
+  (window as any).__Zone_disable_registerElement = true;
+  (window as any).__Zone_disable_EventTargetLegacy = true;
+  (window as any).__Zone_disable_timers = true;
+  (window as any).__Zone_disable_requestAnimationFrame = true;
   (window as any).__Zone_disable_queueMicrotask = true;
+  (window as any).__Zone_disable_blocking = true;
+  (window as any).__Zone_disable_EventTarget = true;
+  (window as any).__Zone_disable_FileReader = true;
+  (window as any).__Zone_disable_MutationObserver = true;
+  (window as any).__Zone_disable_IntersectionObserver = true;
+  (window as any).__Zone_disable_on_property = true;
+  (window as any).__Zone_disable_customElements = true;
+  (window as any).__Zone_disable_XHR = true;
   (window as any).__Zone_disable_geolocation = true;
   (window as any).__Zone_disable_canvas = true;
-  (window as any).__zone_symbol__DISABLE_WRAPPING_UNCAUGHT_PROMISE_REJECTION = true;
-  (window as any).__Zone_disable_timers = true;
-  (window as any).__Zone_disable_XHR = true;
   (window as any).__Zone_disable_ZoneAwarePromise = true;
-  (window as any).__Zone_disable_on_property = true;
-  (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove', 'click', 'mouseover', 'mouseout', 'mouseenter', 'mouseleave', 'resize', 'keypress'];
+  (window as any).__zone_symbol__DISABLE_WRAPPING_UNCAUGHT_PROMISE_REJECTION = true;
+  (window as any).__Zone_disable_jasmine = true;
+  (window as any).__Zone_disable_mocha = true;
+  (window as any).__Zone_disable_jest = true;
+  (window as any).__zone_symbol__fakeAsyncDisablePatchingClock = true;
+  (window as any).__zone_symbol__fakeAsyncAutoFakeAsyncWhenClockPatched = true;
+  (window as any).__zone_symbol__supportWaitUnResolvedChainedPromise = true;
+  (window as any).__zone_symbol__ignoreConsoleErrorUncaughtError = true;
 }
+
+
